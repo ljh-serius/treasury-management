bca562da204fcd547e5b64cd111c6d20428d4998
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-08-11 21:35:11 +0200

    Hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import TransactionBooks from './components/TransactionBooks';
 import Analytics from './components/Analytics';
