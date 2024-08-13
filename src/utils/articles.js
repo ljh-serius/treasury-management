ee3f4056f6180cb3f80cc8fd4fdeec1090102247
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-08-13 21:48:07 +0200

    ok

--- src/utils/articles.js
+++ src/utils/articles.js
@@ -1,9 +1,11 @@
 import article1 from '../articles/the-five-levels-of-treasury-management';
 import article2 from '../articles/quick-overview';
+import article3 from '../articles/sepa-support';
 
 const articles = [
   { slug: 'the-five-levels-of-treasury-management', content: article1, title: "The Five Levels of Treasury Management" },
   { slug: 'quick-overview', content: article2, title: "Quick overview of our software" },
+  { slug: 'sepa-support', content: article3, title: "Supporting SEPA Euro Payments for Streamlined Transactions" },
 ];
 
 export default articles;
