f35a4a81fb26e11998343394bedee659218c28ba
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-08-14 14:16:26 +0200

    Hello world

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,4 @@
 import { onRequest } from "firebase-functions/v2/https";
-import { config } from "firebase-functions";
-import Stripe from "stripe";
-
-// Initialize Stripe with your secret key from Firebase config
-const stripe = new Stripe(config().stripe.secret_key, {
-    apiVersion: "2024-06-20"
-})
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
