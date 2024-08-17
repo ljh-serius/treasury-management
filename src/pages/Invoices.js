656692f573751828101b12f406136a087c29a020
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-08-17 18:44:01 +0200

    ok

--- src/pages/Invoices.js
+++ src/pages/Invoices.js
@@ -10,17 +10,26 @@ import {
   headCells
 } from '../components/BaseManagementComponent/InvoicesConfig';
 
-function InvoicesManagement() {
+import InvoicesAnalysis from './Analysis/Invoices'
+
+function InvoicesManagement({showAnalytics}) {
   return (
-    <BaseManagementComponent
-      fieldConfig={fieldsConfig}
-      entityName={entityName}
-      fetchItems={fetchItems}
-      addItem={addItem}
-      updateItem={updateItem}
-      deleteItem={deleteItem}
-      headCells={headCells} // Pass the dynamically generated head cells
-    />
+    <>
+      {
+        showAnalytics ?
+        ( <InvoicesAnalysis fetchInvoices={fetchItems} /> ) : (
+            <BaseManagementComponent
+            fieldConfig={fieldsConfig}
+            entityName={entityName}
+            fetchItems={fetchItems}
+            addItem={addItem}
+            updateItem={updateItem}
+            deleteItem={deleteItem}
+            headCells={headCells}
+          />
+        )
+      }
+    </>
   );
 }
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -16,16 +16,17 @@ function ProductsManagement({showAnalytics}) {
   return (
     <>
       {
-        showAnalytics ? ( <ProductsAnalysis fetchProducts={fetchItems} /> ) : (
-          <BaseManagementComponent
-          fieldConfig={fieldsConfig}
-          entityName={entityName}
-          fetchItems={fetchItems}
-          addItem={addItem}
-          updateItem={updateItem}
-          deleteItem={deleteItem}
-          headCells={headCells}
-        />
+        showAnalytics ?
+        ( <ProductsAnalysis fetchProducts={fetchItems} /> ) : (
+            <BaseManagementComponent
+            fieldConfig={fieldsConfig}
+            entityName={entityName}
+            fetchItems={fetchItems}
+            addItem={addItem}
+            updateItem={updateItem}
+            deleteItem={deleteItem}
+            headCells={headCells}
+          />
         )
       }
     </>
