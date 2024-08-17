002809c81c79afac9ca15dc03666dde6c45dc292
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-08-17 04:59:10 +0200

    cleaning

--- src/pages/EntityManager.js
+++ src/pages/EntityManager.js
@@ -116,7 +116,6 @@ const ManageEntities = () => {
 
   return (
     <Container maxWidth="xl" sx={{ paddingTop: 3, paddingBottom: 7 }}>
-}}>
       <Divider sx={{ mb: 3 }} />
       <form onSubmit={handleAddEntity}>
         <Grid container spacing={2} sx={{ mb: 3 }}>
